8e4e9373286be5a0c913be1bd54871dd2b2d797e
devhoya97 <diajbh123@gmail.com>
2023-12-06 15:17:25 +0900

    refactor: 게임 재시작 여부 입력메시지의 줄바꿈 설정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
     }
 
     public static boolean doesRetry() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         String input = Console.readLine();
         int parsedInput = customParseInt(input);
