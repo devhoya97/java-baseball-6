02dd02cac3198006baf735b4ff96ff6dd6acf1c3
devhoya97 <diajbh123@gmail.com>
2023-12-06 13:02:00 +0900

    feat: 스트라이크 개수를 계산하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.domain.Baseball;
+import baseball.domain.Computer;
+import baseball.domain.Judgement;
+import baseball.domain.Player;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Baseball player = new Player(List.of(1, 2, 3));
+        Baseball computer = new Computer();
+
+        Judgement judgement = new Judgement();
+        System.out.println("player : " + player.getNumbers());
+        System.out.println("computer : " + computer.getNumbers());
+        System.out.println("strike : " + judgement.calculateStrike(player, computer));
     }
 }
--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -1,15 +1,30 @@
 package baseball.domain;
 
+import static baseball.domain.Constants.BASEBALL_NUMBERS_LENGTH;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Judgement {
-    public int calculateStrike(BaseballNumbers baseballNumbers1, BaseballNumbers baseballNumbers2 ) {
+    public int calculateStrike(Baseball player, Baseball computer) {
+        List<Integer> playerNumbers = player.getNumbers();
+        List<Integer> computerNumbers = computer.getNumbers();
+
+        int strike = 0;
+        for (int baseballIndex = 0; baseballIndex < BASEBALL_NUMBERS_LENGTH; baseballIndex++) {
+            if (Objects.equals(playerNumbers.get(baseballIndex), computerNumbers.get(baseballIndex))) {
+                strike++;
+            }
+        }
+
+        return strike;
+    }
+
+    public int calculateBall(Baseball baseballNumbers1, Baseball baseballNumbers2 ) {
         return 0;
     }
 
-    public int calculateBall(BaseballNumbers baseballNumbers1, BaseballNumbers baseballNumbers2 ) {
-        return 0;
-    }
-
-    public boolean isEnd(BaseballNumbers baseballNumbers1, BaseballNumbers baseballNumbers2 ) {
+    public boolean isEnd(Baseball baseballNumbers1, Baseball baseballNumbers2 ) {
         return false;
     }
 }
