273f9881e54394ef0a5ff1a5fc4932c6259505cf
devhoya97 <diajbh123@gmail.com>
2023-12-06 12:40:17 +0900

    feat: 컴퓨터 숫자를 생성하는 기능 구현

--- src/main/java/baseball/domain/ComputerNumbers.java
+++ src/main/java/baseball/domain/ComputerNumbers.java
@@ -1,5 +1,11 @@
 package baseball.domain;
 
+import static baseball.domain.Constants.BASEBALL_NUMBERS_LENGTH;
+import static baseball.domain.Constants.MAX_BASEBALL_NUMBER;
+import static baseball.domain.Constants.MIN_BASEBALL_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumbers implements BaseballNumbers {
@@ -8,16 +14,21 @@ public class ComputerNumbers implements BaseballNumbers {
         create();
     }
 
-    private void create() {
+    public void create() {
+        List<Integer> randomNumbers = new ArrayList<>();
 
-    }
+        while (randomNumbers.size() < BASEBALL_NUMBERS_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_BASEBALL_NUMBER, MAX_BASEBALL_NUMBER);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
 
-    public void recreate() {
-
+        computerNumbers = randomNumbers;
     }
 
     @Override
     public List<Integer> getNumbers() {
-        return null;
+        return computerNumbers;
     }
 }
---
+++ src/main/java/baseball/domain/Constants.java
@@ -0,0 +1,7 @@
+package baseball.domain;
+
+public abstract class Constants {
+    public static final int BASEBALL_NUMBERS_LENGTH = 3;
+    public static final int MIN_BASEBALL_NUMBER = 1;
+    public static final int MAX_BASEBALL_NUMBER = 9;
+}
